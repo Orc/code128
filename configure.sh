5d445b1c3611b6095d116583ec39347410f455d8
david parsons <orc@pell.portland.or.us>
2017-04-10 21:56:41 -0700

    update configure.inc to the latest & greatest; update configure.sh to work with it because despite my best efforts the interface has drifted over the past 20 years

--- configure.sh
+++ configure.sh
@@ -11,60 +11,25 @@ TARGET=code128
 
 . ./configure.inc
 
-
-find_gd() {
-    gd_found=1
-    if AC_QUIET AC_CHECK_HEADERS gd.h $1; then
-	AC_CFLAGS="$AC_CFLAGS -I$1"
-	AC_LDFLAGS="$AC_LDFLAGS -L$1"
-	AC_INCLUDE '<gd.h>'
-	LOG "gd.h found in $1"
-    elif AC_QUIET AC_CHECK_HEADERS gd/gd.h $1; then
-	AC_CFLAGS="$AC_CFLAGS -I$1"
-	AC_LDFLAGS="$AC_LDFLAGS -L$1"
-	AC_INCLUDE '<gd/gd.h>'
-	LOG "gd.h found in $1/gd"
-    elif AC_QUIET AC_CHECK_HEADERS gd.h $1/include; then
-	AC_CFLAGS="$AC_CFLAGS -I$1/include"
-	AC_LDFLAGS="$AC_LDFLAGS -L$1/lib"
-	AC_INCLUDE '<gd.h>'
-	LOG "gd.h found in $1/include"
-    elif AC_QUIET AC_CHECK_HEADERS gd/gd.h $1/include; then
-	AC_CFLAGS="$AC_CFLAGS -I$1/include"
-	AC_LDFLAGS="$AC_LDFLAGS -L$1/lib"
-	AC_INCLUDE '<gd/gd.h>'
-	LOG "gd.h found in $1/include/gd"
-    fi
-}
-
-
 AC_INIT $TARGET
 
-test "x$WITH_GD"  != "x" && find_gd $WITH_GD
+case "$AC_LDFLAGS" in
+*-lm*) ;;
+*) AC_LDFLAGS="$AC_LDFLAGS -lm" ;;
+esac
 
-if [ "$OS_FREEBSD" -a -z "$gd_found" ]; then
-    find_gd /usr/local
+if AC_CHECK_HEADERS gd.h; then
+    AC_TEXT "#include <gd.h>"
+elif AC_CHECK_HEADERS gd/gd.h; then
+    AC_TEXT "#include <gd/gd.h>"
+else
+    exit 1
 fi
 
-if [ "$gd_found" ]; then
-    AC_LIBRARY gdImageCreate -lgd || unset gd_found
-fi
-
-if [ -z "$gd_found" ]; then
-    LOG "Cannot find libgd;  you need to have a copy of Tom Boutell's"
-    LOG "GD library before $TARGET will work.  You can get gd from"
-    AC_FAIL "http://www.boutell.com/gd"
-fi
-
-if AC_LIBRARY jpeg_destroy -ljpeg; then
-    AC_LIBS="$AC_LIBS -ljpeg"
-    LOG "Using JPG format"
-elif AC_CHECK_FUNCS gdImageGif; then
+if AC_LIBRARY gdImageGif -lgd; then
     AC_DEFINE GD_SUPPORTS_GIF 1
 else
-    LOG "There is no libjpeg on this system and the copy of"
-    AC_FAIL "libgd here does not support the GIF format."
+    AC_FAIL "The copy of libgd here does not support the GIF format."
 fi
 
 AC_OUTPUT Makefile
-
